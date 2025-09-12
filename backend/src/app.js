8a670dafae7ab7b90e364b13ed06fb7aa5c00bbb
Janhawi K <kjanhawi357@gmail.com>
2025-09-12 16:56:33 +0530

    files added

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,11 @@ app.use(cookieParser());
 // Import All Routes
 import userRoute from "./routes/userRoutes.js";
 import vehicleRoute from "./routes/vehicleRoutes.js";
+import driverRoute from "./routes/DriverRoutes.js";
 
 app.use("/user",userRoute); //User Routes
 app.use("/vehicle",vehicleRoute); //Vehicle Routes
+app.use("/driver",driverRoute);  // Driver Routes
 
 // global error handling
 app.use(errorHandler);
