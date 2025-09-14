7e99db193d5bff5f2cebf8999a82f16672124306
DhavalBavda <107936688+DhavalBavda@users.noreply.github.com>
2025-09-14 17:38:46 +0530

    ts files1

---
+++ CPP App/hiredis1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/TRAINING/C++ Code Practice/hiredis
+# Build directory: C:/TRAINING/C++ Code Practice/hiredis/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "C:/TRAINING/C++ Code Practice/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "C:/TRAINING/C++ Code Practice/hiredis/CMakeLists.txt;237;ADD_TEST;C:/TRAINING/C++ Code Practice/hiredis/CMakeLists.txt;0;")
